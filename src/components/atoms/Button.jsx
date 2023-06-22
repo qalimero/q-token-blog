b180c27cb16da41755d8fe0ebee692fdafe0c2a6
q.serda <quentinserda@gmail.com>
2023-06-22 18:35:30 +0200

    first commit

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,10 @@
+export default function Button() {
+    return (
+        <>
+            <div className="a-button">
+                <h1>Welcome to my blog</h1>
+                <p>Test to show my first component</p>
+            </div>
+        </>
+    );
+}
